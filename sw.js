b0d4f37c6c5108ce9052e9ff8360288c13d020c8
njfilmes <njfilmes1@gmail.com>
2026-08-26 16:58:14 +0000

    Publicar Antonio Junior Hub pessoal
    Versão separada do Manus com login local, rotina gamificada, finanças editáveis, gráficos, manifest e suporte offline.

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'antonio-junior-hub-v1'
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.svg']
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone()
+    caches.open(CACHE).then(cache => cache.put(event.request, copy))
+    return response
+  }).catch(() => caches.match('./index.html'))))
+})
