a4c25129f4a490395c514180593e3acb6ca49410
njfilmes <njfilmes1@gmail.com>
2026-08-26 18:53:06 +0000

    Forçar cache v4 da versão limpa
    Atualiza os service workers para v4, substituindo definitivamente o cache antigo que ainda podia servir a página branca.

--- sw-v2.js
+++ sw-v2.js
@@ -1,4 +1,4 @@
-const CACHE = 'antonio-junior-hub-v2'
+const CACHE = 'antonio-junior-hub-v4'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.svg']
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)))
