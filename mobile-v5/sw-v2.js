1410231b2f2d1fb8eef9936f1c09531a77ba51f5
njfilmes <njfilmes1@gmail.com>
2026-08-27 00:12:46 -0300

    Usar novo retrato em preto e branco no mobile-v5
    Atualiza favicon, manifestos, workers e ícones instaláveis da versão mobile-v5 para o novo retrato v4.

---
+++ mobile-v5/sw-v2.js
@@ -0,0 +1,19 @@
+const CACHE = 'antonio-junior-hub-v10-photo-v4'
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './antonio-junior-photo-banner-v3.webp', './antonio-junior-photo-icon-64-v4.png', './antonio-junior-photo-icon-180-v4.png', './antonio-junior-photo-icon-192-v4.png', './antonio-junior-photo-icon-512-v4.png', './antonio-junior.webp']
+self.addEventListener('install', event => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL).catch(() => undefined)))
+})
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()))
+})
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  const url = new URL(event.request.url)
+  if (url.origin !== self.location.origin) return
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone()
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => undefined)
+    return response
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))))
+})
